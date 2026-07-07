f2f746bcde7c7b632a6dd3ca998505c3c05aa84c
RAJESH VEERAMALLU <rajesh45v@gmail.com>
2026-07-07 21:11:36 +0530

    Create install-eks-prerequisites.sh

---
+++ install-eks-prerequisites.sh
@@ -0,0 +1,32 @@
+# Step 1: Permission Eevadaniki
+```
+chmod +x install-eks-prerequisites.sh
+```
+
+# Step 2: Run cheyyallii
+```
+./install-eks-prerequisites.sh
+```
+```
+aws --version
+terraform --version
+git --version
+```
+```
+terraform init
+```
+```
+terraform validate
+```
+```
+terraform fmt
+```
+```
+terraform plan
+```
+```
+terraform apply --auto-approve
+```
+```
+terraform destroy --auto-approve
+```
